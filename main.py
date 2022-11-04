2e30be9a9af9b52fd2d93a32b5d93bd94ff6a6c6
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-03 21:58:42 -0400

    removed dup lettering from alphabet from testing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import art
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def caesar(text, shift, direction):
     end_text = ""
     if direction == "decode":
