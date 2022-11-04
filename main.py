4deea11dd9b591f541c59f5d5262583716b2ec38
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-03 21:54:22 -0400

    added prompt to loop program

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-print(art.logo)
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-user_shift = int(input("Type the secret number:\n"))
-
-# Allows for any number to be used as shift
-shift = user_shift % 26
-
 def caesar(text, shift, direction):
     end_text = ""
     if direction == "decode":
@@ -21,11 +12,29 @@ def caesar(text, shift, direction):
             new_position = position + shift
             end_text += alphabet[new_position]
         else: 
-            end_text += char
-            
+            end_text += char      
     print(f"The {direction}d text is {end_text}") 
-       
-caesar(text, shift, direction)
+
+
+
+restart = True
+while restart:
+    print(art.logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    user_shift = int(input("Type the secret number:\n"))
+    # Allows for any number to be used as shift
+    shift = user_shift % 26
+
+    caesar(text, shift, direction)
+
+    restart_prompt = input("Would you like to use Caesar Cipher again? Type 'yes' or 'no'.\n")
+    if restart_prompt == "no":
+        restart = False
+        print("Goodbye!")
+
+
+
 
 # Non Refactored Version:
 """
