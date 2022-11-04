a25d578ee2dab02d588fae3c868334383acb5e91
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-03 21:15:05 -0400

    refactored into single function

--- main.py
+++ main.py
@@ -5,27 +5,41 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the secret number:\n"))
 
-def encrypt(text, shift):
-    cipher = ""
-    for letter in text: 
-        letter_index = int(alphabet.index(letter) + shift)   
-        new_letter = alphabet[letter_index]
-        cipher += new_letter
-    print(f"The encoded text is {cipher}")   
+def caesar(text, shift, direction):
+    end_text = ""
+    if direction == "decode":
+        shift *= -1
     
-def decrypt(text, shift):
-    plain_text = ""
-    for letter in text: 
-        letter_index = int(alphabet.index(letter) - shift)   
-        new_letter = alphabet[letter_index]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}") 
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        end_text += alphabet[new_position]
+    print(f"The {direction}d text is {end_text}") 
+    
+caesar(text, shift, direction)
 
+#def encrypt(text, shift):
+#    cipher = ""
+#    for letter in text: 
+#        letter_index = int(alphabet.index(letter) + shift)   
+#        new_letter = alphabet[letter_index]
+#        cipher += new_letter
+#    print(f"The encoded text is {cipher}")   
+#    
+#def decrypt(text, shift):
+#    plain_text = ""
+#    for letter in text: 
+#        letter_index = int(alphabet.index(letter) - shift)   
+#        new_letter = alphabet[letter_index]
+#        plain_text += new_letter
+#    print(f"The decoded text is {plain_text}") 
+
+#if direction == "encode":
+#    encrypt(text, shift)
+#elif direction == "decode":
+#    decrypt(text, shift)
   
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+
 
 
     
