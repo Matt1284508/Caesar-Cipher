5427d77f6460f4c1bdb763ccd4122acea8d54692
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-15 22:42:09 -0500

    added gif file and small formatting changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 def caesar(text, shift, direction):
     end_text = ""
     if direction == "decode":
